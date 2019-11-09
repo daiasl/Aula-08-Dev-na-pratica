f6cdaff38d6d5ab9e5fde8e4f7b5337cedf833a7
Brenon Araujo <brenonaraujo@gmail.com>
2019-11-09 00:26:21 -0300

    aula 2 setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,14 @@ import { AppComponent } from './app.component';
 import { SideNavComponent } from './shared/common/side-nav/side-nav.component';
 import { TopBarComponent } from './shared/common/top-bar/top-bar.component';
 import { FeaturesModule } from './features/features.module';
-import { PermissionsModule } from '@seniorsistemas/platform-components';
 import { TranslateModule } from '@ngx-translate/core';
 import { HotkeyModule } from 'angular2-hotkeys';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GrowlModule } from 'primeng/growl';
 import {
-    HttpInterceptorModule,
     CyclicJsonInterceptorModule,
     LocaleModule,
-    RestUrl,
     BreadcrumbModule,
     LoadingStateModule,
     ProductHeaderModule,
--- src/app/features/features.routing.ts
+++ src/app/features/features.routing.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{
-  path: 'clientes', redirectTo: 'cliente/list'
-},
-{
-  path: 'items', redirectTo: 'item/list'
-}];
+const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
